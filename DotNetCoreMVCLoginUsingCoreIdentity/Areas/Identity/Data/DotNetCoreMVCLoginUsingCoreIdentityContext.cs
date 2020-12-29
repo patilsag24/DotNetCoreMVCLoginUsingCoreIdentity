3ee746b56afc62dffaba19ce4ca80d5fb29274a9
Sagar Patil <Sagar.Patil1@blueconchtech.com>
2020-12-29 17:45:16 +0530

    adding files

---
+++ DotNetCoreMVCLoginUsingCoreIdentity/Areas/Identity/Data/DotNetCoreMVCLoginUsingCoreIdentityContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreMVCLoginUsingCoreIdentity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreMVCLoginUsingCoreIdentity.Data
+{
+    public class DotNetCoreMVCLoginUsingCoreIdentityContext : IdentityDbContext<DotNetCoreMVCLoginUsingCoreIdentityUser>
+    {
+        public DotNetCoreMVCLoginUsingCoreIdentityContext(DbContextOptions<DotNetCoreMVCLoginUsingCoreIdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ DotNetCoreMVCLoginUsingCoreIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DotNetCoreMVCLoginUsingCoreIdentity.Areas.Identity.Data;
+using DotNetCoreMVCLoginUsingCoreIdentity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DotNetCoreMVCLoginUsingCoreIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace DotNetCoreMVCLoginUsingCoreIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DotNetCoreMVCLoginUsingCoreIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DotNetCoreMVCLoginUsingCoreIdentityContextConnection")));
+
+                services.AddDefaultIdentity<DotNetCoreMVCLoginUsingCoreIdentityUser>(options => {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                    .AddEntityFrameworkStores<DotNetCoreMVCLoginUsingCoreIdentityContext>();
+            });
+        }
+    }
+}
